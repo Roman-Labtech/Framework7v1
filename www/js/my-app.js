416ee79a823da2a280df66bad5e2cd2d880f8ea5
Roman-Labtech <labtechno.roman@yandex.ru>
2018-02-20 08:29:17 +0300

    Add files via upload

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,5 +1,8 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    pushState: true,
+    // ... other parameters
+});
 
 // Export selectors engine
 var $$ = Dom7;
@@ -194,4 +197,32 @@ if (isFinite(c) &&(c>0 && c<180)){
 	myApp.alert('Треугольник с такими сторонами не существует. Введите корректные значения');
 }};
  
-
+var app = {
+    checkSMSPermission: function() {
+        var success = function (hasPermission) { 
+            if (hasPermission) {
+                sms.send(...);
+            }
+            else {
+                // show a helpful message to explain why you need to require the permission to send a SMS
+                // read http://developer.android.com/training/permissions/requesting.html#explain for more best practices
+            }
+        };
+        var error = function (e) { alert('Something went wrong:' + e); };
+        sms.hasPermission(success, error);
+    },
+    requestSMSPermission: function() {
+        var success = function (hasPermission) { 
+            if (!hasPermission) {
+                sms.requestPermission(function() {
+                    console.log('[OK] Permission accepted')
+                }, function(error) {
+                    console.info('[WARN] Permission not accepted')
+                    // Handle permission not accepted
+                })
+            }
+        };
+        var error = function (e) { alert('Something went wrong:' + e); };
+        sms.hasPermission(success, error);
+    }
+};
